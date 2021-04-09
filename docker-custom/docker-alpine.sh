9e52de7a90b68b8994d7954f8a904b59cf203fbc
Anson <278281063@qq.com>
2021-04-09 15:38:18 +0800

    还原docker-alpine.sh

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -8,11 +8,9 @@ echo "#########################################################################"
 docker build --rm --no-cache \
     --build-arg ARCH=amd64 \
     --build-arg NODE_VERSION=12 \
-    --build-arg NODE_RED_VERSION=1.2.7 \
+    --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
     --build-arg OS=alpine \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.custom \
     --tag testing:node-red-build .
-
-exec /bin/bash
